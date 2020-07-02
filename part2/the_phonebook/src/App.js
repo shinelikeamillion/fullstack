ba9858ba7646a211cf840c0c5d46591fbd788d29
lilsean <geeks.liu@gmail.com>
2020-07-02 20:23:00 +0800

    Exercises 2.11 json_server axios useEffect

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, onChange }) =>
     <p>filter shown with: <input value={filter} onChange={onChange} /> </p>
@@ -28,12 +29,7 @@ const NumbersTable = ({ filter, persons }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
@@ -55,6 +51,16 @@ const App = () => {
         event.preventDefault()
         addPerson()
     }
+
+    // useEffect function must return a cleanup function or nothing
+    const hook = () => {
+        (async () => {
+            let response = await axios.get('http://localhost:3001/persons')
+            if (response.status === 200) setPersons(response.data)
+        })()
+    }
+
+    useEffect(hook, []);
 
     return (
         <div>
