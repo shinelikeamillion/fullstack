fb1fcdd1b1d6d5582652b2047a17132909370243
lilsean <geeks.liu@gmail.com>
2020-07-03 20:21:04 +0800

    Exercises 2.13-2.14. read app_id from .env fetch weather data from open API

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Countries from './components/Countries'
 
 const App = () => {
     const [name, setName] = useState('china')
     const [countries, setCountries] = useState([])
 
     // useEffect function must return a cleanup function or nothing
-    const hook = () => {
+    const hook = async () => {
         if (!name) return
-        (async () => {
-            await axios
-                .get(`https://restcountries.eu/rest/v2/name/${name}`)
-                .then(response => {
-                    if (response.status === 200) handleResult(response.data)
-                })
-                .catch((err) => console.log(err))
-        })()
+        await axios
+            .get(`https://restcountries.eu/rest/v2/name/${name}`)
+            .then(response => {
+                if (response.status === 200) handleResult(response.data)
+            })
+            .catch((err) => console.log(err))
     }
 
     function handleResult(data) {
@@ -28,7 +27,7 @@ const App = () => {
         }
     }
 
-    useEffect(hook, [name]);
+    useEffect(() => { hook() }, [name]);
 
     return (
         <div>
@@ -37,37 +36,5 @@ const App = () => {
         </div>
     )
 }
-
-const Countries = ({ countries }) => {
-    if (countries.length > 10) {
-        return <p>Too many matches, specify another filter</p>
-    } else if (countries.length > 1) {
-        return countries.map(country => <CountryItem key={country.name} country={country} />)
-    } else if (countries.length === 1) {
-        return <Country country={countries[0]} />
-    }
-    return null
-}
-
-const CountryItem = ({ country }) => {
-    const [showDetail, setShowDetail] = useState(false)
-    return <div>
-        {country.name}
-        <button onClick={() => { setShowDetail(!showDetail) }}>{showDetail ? 'hide' : 'show'}</button>
-        {showDetail ? <Country country={country} /> : null}
-    </div>
-}
-
-const Country = ({ country }) =>
-    <div>
-        <h2>{country.name}</h2>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h3>Languages</h3>
-        <ul>
-            {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
-        </ul>
-        <img width='200' src={country.flag} alt={`${country.name}'s flag`} />
-    </div>
 
 export default App
---
+++ part2/data_for_countries/src/components/Countries.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const CountryWeather = ({ weather }) => weather
+    ?
+    <>
+        <h3>Weather in {weather.country}</h3>
+        <div>temperature: {weather.temp} Celsius</div>
+        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={weather.description} />
+        <div>wind: {weather.wind.speed} mps direction {weather.wind.deg} deg</div>
+    </>
+    :
+    null
+
+const Countries = ({ countries }) => {
+
+    if (!process.env.REACT_APP_URL || !process.env.REACT_APP_ID) {
+        console.error(
+            ```
+            Note that you need to apply your own app_id from https://openweathermap.org
+            or you can use the official Examples API below for a glance
+            api.openweathermap.org/data/2.5/weather?q=London,uk
+            ```
+        )
+    }
+
+    if (countries.length > 10) {
+        return <p>Too many matches, specify another filter</p>
+    } else if (countries.length > 1) {
+        return countries.map(country => <CountryItem key={country.name} country={country} />)
+    } else if (countries.length === 1) {
+        return <Country country={countries[0]} />
+    }
+    return null
+}
+
+const CountryItem = ({ country }) => {
+    const [showDetail, setShowDetail] = useState(false)
+    return <div>
+        {country.name}
+        <button onClick={() => { setShowDetail(!showDetail) }}>{showDetail ? 'hide' : 'show'}</button>
+        {showDetail ? <Country country={country} /> : null}
+    </div>
+}
+
+const Country = ({ country }) => {
+    const [weather, setWeather] = useState()
+
+    const getWeather = async () => await
+        axios.get(`${process.env.REACT_APP_URL}?q=${country.name}&units=metric&appid=${process.env.REACT_APP_ID}`)
+            .then(response => {
+                if (response.status === 200) {
+                    let data = response.data
+                    setWeather(
+                        {
+                            country: country.name,
+                            temp: data.main.temp,
+                            wind: data.wind,
+                            icon: data.weather[0].icon,
+                            description: data.weather[0].description
+                        }
+                    )
+                }
+            })
+
+    useEffect(() => { getWeather() }, [])
+
+    return <div>
+        <h2>{country.name}</h2>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>Languages</h3>
+        <ul>
+            {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
+        </ul>
+        <img width='200' src={country.flag} alt={`${country.name}'s flag`} />
+        <CountryWeather weather={weather} />
+    </div>
+}
+
+export default Countries
