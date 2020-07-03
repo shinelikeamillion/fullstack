4429c025cca3825780b91f024b1514aca180f1d5
lilsean <geeks.liu@gmail.com>
2020-07-03 17:34:28 +0800

    Exercises: 2.13*: Data for countries show detail button

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-    const [name, setName] = useState('australia')
+    const [name, setName] = useState('china')
     const [countries, setCountries] = useState([])
 
     // useEffect function must return a cleanup function or nothing
@@ -42,21 +42,32 @@ const Countries = ({ countries }) => {
     if (countries.length > 10) {
         return <p>Too many matches, specify another filter</p>
     } else if (countries.length > 1) {
-        return countries.map(country => <p key={country}>{country}</p>)
+        return countries.map(country => <CountryItem key={country.name} country={country} />)
     } else if (countries.length === 1) {
-        let country = countries[0]
-        return <div>
-            <h2>{country.name}</h2>
-            <p>capital {country.capital}</p>
-            <p>population {country.population}</p>
-            <h3>Languages</h3>
-            <ul>
-                {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
-            </ul>
-            <img width='200' src={country.flag} />
-        </div>
+        return <Country country={countries[0]} />
     }
-    return <></>
+    return null
 }
+
+const CountryItem = ({ country }) => {
+    const [showDetail, setShowDetail] = useState(false)
+    return <div>
+        {country.name}
+        <button onClick={() => { setShowDetail(!showDetail) }}>{showDetail ? 'hide' : 'show'}</button>
+        {showDetail ? <Country country={country} /> : null}
+    </div>
+}
+
+const Country = ({ country }) =>
+    <div>
+        <h2>{country.name}</h2>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>Languages</h3>
+        <ul>
+            {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
+        </ul>
+        <img width='200' src={country.flag} alt={`${country.name}'s flag`} />
+    </div>
 
 export default App
