f164060a5341b36589fa21157247be50c39d4367
lilsean <geeks.liu@gmail.com>
2020-07-16 16:30:18 +0800

    mix reducer

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { vote } from '../reducers/anecdoteReducer'
+import { vote, filter } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(
-    (state) => state.sort((first, second) => second.votes - first.votes),
+    (state) => state.anecdotes
+      .filter((anecdote) => anecdote.content.includes(state.filter))
+      .sort((first, second) => second.votes - first.votes),
   )
   const dispatch = useDispatch()
 
-  return anecdotes.map((anecdote) => (
-    <div key={anecdote.id}>
+  return (
+    <>
       <div>
-        {anecdote.content}
+        <div>Filter:</div>
+        <input onChange={(e) => { dispatch(filter(e.target.value)) }} />
       </div>
-      <div>
-        {`has ${anecdote.votes}`}
-        <button type="button" onClick={() => { dispatch(vote(anecdote.id)) }}>vote</button>
-      </div>
-    </div>
-  ))
+      {
+        anecdotes.map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              {`has ${anecdote.votes}`}
+              <button type="button" onClick={() => { dispatch(vote(anecdote.id)) }}>vote</button>
+            </div>
+          </div>
+        ))
+      }
+    </>
+  )
 }
 
 export default AnecdoteList
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,14 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
+const anecdotesAtStart = {
+  anecdotes: [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ],
+  filter: '',
+}
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -15,19 +18,28 @@ const asObject = (anecdote) => ({
   votes: 0,
 })
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = {
+  ...anecdotesAtStart,
+  anecdotes: anecdotesAtStart.anecdotes.map((str) => asObject(str)),
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
   case 'VOTE': {
     const { id } = action.data
-    return state.map((anecdote) => (anecdote.id === id
-      ? { ...anecdote, votes: anecdote.votes + 1 }
-      : anecdote))
+    return {
+      ...state,
+      anecdotes: state.anecdotes.map((anecdote) => (anecdote.id === id
+        ? { ...anecdote, votes: anecdote.votes + 1 }
+        : anecdote)),
+    }
   }
   case 'CREATE': {
     const newOne = action.data
-    return [...state, newOne]
+    return { ...state, anecdotes: [...state.anecdotes, newOne] }
+  }
+  case 'FILTER': {
+    return { ...state, filter: action.data }
   }
   default: return state
   }
@@ -39,3 +51,4 @@ export const createNew = (content) => {
   const anecdote = asObject(content)
   return { type: 'CREATE', data: anecdote }
 }
+export const filter = (key) => ({ type: 'FILTER', data: key })
