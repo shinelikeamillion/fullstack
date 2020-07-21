7a12a47fb0e2bc2a315e18746cc284c874c5e460
lilsean <geeks.liu@gmail.com>
2020-07-21 16:25:03 +0800

    Exercises 9.8.-9.9.

---
+++ part9/patientor/server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors  from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong')
+})
+
+app.listen(3001, () => {
+  console.log('Server running on port 3001')
+})
+
+
