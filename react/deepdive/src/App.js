ccfdafe308c8cebea2d53b0da9a5a1e114ba8d74
Sean Liu <geeks.liu@gmail.com>
2024-05-16 00:28:38 +1000

    deep dive into react

---
+++ react/deepdive/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ProfilePage } from './suspense/page';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/deepdive/src/fiber/index.js
@@ -0,0 +1,10 @@
+import React from "./react";
+import ReactDOM from "./react-dom";
+
+const rootElement = document.getElementById("root");
+window.ReactDOM.render(
+  <h3>
+    <p>hello ka song~</p>
+  </h3>,
+  rootElement
+);
